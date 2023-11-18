8b1584744fcc40b21badb62aa541563fdd23932e
anoduck <11767-anoduck@users.noreply.gitgud.io>
2023-11-18 00:14:03 -0500

    Debugging trio

--- vidsiv.py
+++ vidsiv.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
-from os.path import isfile
 from simple_parsing import parse, choice
 from alive_progress import alive_bar
 import ffmpeg
@@ -23,7 +22,7 @@ class Options:
     rm: bool = False  # Enable deletion of low quality files.
     noz: bool = False  # Disable removal of zerosum files.
     min: int = 512  # Minimum file size in kilobytes, less than is considered zero sum.
-    lev: str = choice('INFO', 'DEBUG', default='DEBUG')  # Set log level to either INFO or DEBUG
+    lev: str = choice('info', 'debug', default='debug')  # Set log level to either INFO or DEBUG
     log: str = os.path.abspath('./vidsiv.log')  # Full path to log file.
 
 
@@ -34,10 +33,11 @@ def get_log(Options):
     log = logging.getLogger(__name__)
     if log.hasHandlers():
         log.handlers.clear()
-    log_level = Options.lev
-    lev_string = 'logging' + '.' + log_level
-    log.setLevel(lev_string)
-    handler = logging.FileHandler(Options.log_file, mode='a', encoding='utf-8')
+    if Options.lev == 'info':
+        log.setLevel(logging.INFO)
+    else:
+        log.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(Options.log, mode='a', encoding='utf-8')
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     log.addHandler(handler)
@@ -46,37 +46,19 @@ def get_log(Options):
     return log
 
 
-async def get_flist(Options, log):
-    filelst = []
-    flst = []
-    log.info('Generating file list recursively')
-    dir_path = os.path.abspath(Options.dir)
-    log.debug('Directory path: {}'.format(dir_path))
-    for entry in trio.Path(dir_path).rglob('**/*'):
-        if await trio.Path(entry).is_file():
-            filelst.append(entry)
-    log.info('Raw file list generated')
-    log.info('Checking for mimetypes')
-    for item in filelst:
-        mt = mimetypes.guess_type(item)
-        mtrtn = str(mt[0])
-        tsplit = mtrtn.split('/')
-        if tsplit[0] == 'video':
-            flst.append(item)
-    log.info('File list filtered by mimetype.')
-    log.info('The file list has been generated.')
-    return flst
+async def find_videos(recv, sendproc):
+    async with recv:
+        async for item in recv:
+            mt = mimetypes.guess_type(item)
+            mime_str = str(mt[0])
+            tsplit = mime_str('/')
+            if tsplit[0] == 'video':
+                sendproc.send(item)
 
 
-async def main(Options):
-    log = get_log(Options)
-    qty = Options.qty
-    rdict = {'480': 480, '720': 720, '2k': 1920, '4k': 3840}
-    tres = rdict.get(qty)
-    flst = await get_flist(Options, log)
-    total = len(flst)
-    with alive_bar(total) as bar:
-        for item in flst:
+async def proc_vids(recvproc, log, tres, Options, bar):
+    async with recvproc:
+        async for item in recvproc:
             log.debug('Item is: {}'.format(item))
             fsize = os.path.getsize(item)
             min_size = Options.min * 1024
@@ -86,7 +68,7 @@ async def main(Options):
                 log.info('Zero sum item {} removed.'.format(item))
                 bar()
                 continue
-            ffdict= ffmpeg.probe(item)
+            ffdict = ffmpeg.probe(item)
             ffstreams = ffdict.get('streams')
             vidstream = ffstreams[0]
             fwidth = vidstream.get('width')
@@ -107,7 +89,49 @@ async def main(Options):
                     if Options.rm:
                         os.remove(item)
                         log.info('Deleting {}'.format(item))
-            bar()
+                        bar()
+
+
+async def get_flist(Options, sendproc, log):
+    sendflist, recflist = trio.open_memory_channel(0)
+    log.info('Generating file list recursively')
+    dir_path = os.path.abspath(Options.dir)
+    log.debug('Directory path: {}'.format(dir_path))
+    for entry in trio.Path(dir_path).rglob('**/*'):
+        if await trio.Path(entry).is_file():
+            sendflist.send(entry)
+    log.info('Raw file list generated')
+    log.info('Checking for mimetypes')
+    sendproc1 = sendproc
+    sendproc2 = sendproc.clone()
+    sendproc3 = sendproc.clone()
+    recv1 = recflist
+    recv2 = recflist.clone()
+    recv3 = recflist.clone()
+    async with trio.open_nursery() as nsy:
+        nsy.start_soon(find_videos, recv=recv1, sendproc=sendproc1)
+        nsy.start_soon(find_videos, recv=recv2, sendproc=sendproc2)
+        nsy.start_soon(find_videos, recv=recv3, sendproc=sendproc3)
+    await trio.sleep(0)
+    log.info('File list filtered by mimetype.')
+    log.info('The file list has been generated.')
+
+
+async def main(Options):
+    log = get_log(Options)
+    qty = Options.qty
+    sendproc, recvproc = trio.open_memory_channel(0)
+    rdict = {'480': 480, '720': 720, '2k': 1920, '4k': 3840}
+    tres = rdict.get(qty)
+    await get_flist(Options, sendproc, log)
+    total = recvproc.qsize()
+    with alive_bar(total) as bar:
+        async with trio.open_nursery as nsy0:
+            nsy0.start_soon(proc_vids, recvproc=recvproc, log=log, tres=tres, Options=Options, bar=bar)
+            nsy0.start_soon(proc_vids, recvproc=recvproc.clone(), log=log, tres=tres, Options=Options, bar=bar)
+            nsy0.start_soon(proc_vids, recvproc=recvproc.clone(), log=log, tres=tres, Options=Options, bar=bar)
+            nsy0.start_soon(proc_vids, recvproc=recvproc.clone(), log=log, tres=tres, Options=Options, bar=bar)
+    await trio.sleep(0) 
     log.info('{} files processed'.format(total))
     log.info('Process complete.')
     print('Done!')
